2e78924c71bdfe98bc7ee7f74f66418a678d986f
kartikeya-m1 <kartikeyamudliyar@gmail.com>
2025-09-29 02:40:55 +0530

    added dark mode in search and filter map view

--- src/features/core/SearchFilters.tsx
+++ src/features/core/SearchFilters.tsx
@@ -488,10 +488,16 @@ const SearchFilters: React.FC = () => {
         zoomControl={true}
       >
         <LayersControl position="topright">
-          <LayersControl.BaseLayer checked name="Ocean View">
+          <LayersControl.BaseLayer name="Ocean View">
             <TileLayer
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer checked name="Dark Mode">
+            <TileLayer
+              url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
             />
           </LayersControl.BaseLayer>
           <LayersControl.BaseLayer name="Satellite">
